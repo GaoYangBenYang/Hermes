350b67bff3d20d326c4402eb2cc64bff7df7f2f0
GaoYang <GaoYangBenYang@outlook.com>
2023-04-10 17:51:43 +0800

    🎨 修改目录结构

---
+++ api/router.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"ProblemFocus_Backend/internal/controller"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+		beego.NSNamespace("/object",
+			beego.NSInclude(
+				&controllers.ObjectController{},
+			),
+		),
+		beego.NSNamespace("/user",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "ProblemFocus_Backend/api"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run("localhost")
+}
