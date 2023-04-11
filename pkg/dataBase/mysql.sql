8c181ec167cb88fb36b1356c2ddeabd3ef79d319
GaoYang <GaoYangBenYang@outlook.com>
2023-04-11 12:38:18 +0800

    🔧 添加comment 修改部份表 剩余表需要时修改

--- pkg/dataBase/mysql.sql
+++ pkg/dataBase/mysql.sql
@@ -1,35 +1,40 @@
+-- Active: 1679220809744@@127.0.0.1@3306@problemfocus
+
+CREATE DATABASE ProblemFocus
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
 -- 1.用户模块
 CREATE TABLE user (
-  user_id INT AUTO_INCREMENT PRIMARY KEY, COMMENT "用户ID,唯一标识符,自增长整数类型"
-  username VARCHAR(50) NOT NULL UNIQUE, COMMENT "用户名，字符串类型，不可重复"
-  password VARCHAR(255) NOT NULL,COMMENT "密码，字符串类型，加密存储"
-  email VARCHAR(255), COMMENT "电子邮件地址，字符串类型"
-  phone_number VARCHAR(20),COMMENT "电话号码，字符串类型"
-  avatar VARCHAR(255), COMMENT "头像,字符串类型,存储头像的URL地址。"
-  registration_time DATETIME COMMENT "注册时间，日期时间类型"
-  updated_time DATETIME COMMENT "用户更新时间，可为空"
+  user_id INT AUTO_INCREMENT PRIMARY KEY COMMENT "用户ID,唯一标识符,自增长整数类型",
+  username VARCHAR(50) NOT NULL UNIQUE COMMENT "用户名，字符串类型，不可重复",
+  password VARCHAR(255) NOT NULL COMMENT "密码，字符串类型，加密存储",
+  email VARCHAR(255) COMMENT "电子邮件地址，字符串类型",
+  phone_number VARCHAR(20) COMMENT "电话号码，字符串类型",
+  avatar VARCHAR(255) COMMENT "头像,字符串类型,存储头像的URL地址",
+  registration_time DATETIME COMMENT "注册时间，日期时间类型",
+  update_time DATETIME COMMENT "用户更新时间，可为空"
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT "用户表";
 
 -- 2.提问模块
 CREATE TABLE question (
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, COMMENT "问题ID,主键,自增长"
-title VARCHAR(255) NOT NULL, COMMENT "问题标题，不为空"
-description TEXT,COMMENT "问题描述，可为空"
-code TEXT, COMMENT "问题代码，可为空"
-language VARCHAR(50), COMMENT "问题代码语言，可为空"
-views INT UNSIGNED NOT NULL DEFAULT 0, COMMENT "问题浏览次数,不为空,默认值为0"
-votes INT UNSIGNED NOT NULL DEFAULT 0, COMMENT "问题投票次数,不为空,默认值为0"
-created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, COMMENT "问题创建时间，不为空"
-updated_at TIMESTAMP, COMMENT "问题更新时间，可为空"
-user_id INT UNSIGNED NOT NULL, COMMENT "问题发布者ID,外键,关联用户表中的id字段"
-status TINYINT UNSIGNED NOT NULL DEFAULT 0, COMMENT "问题状态,不为空,默认值为0,0表示未解决,1表示已解决"
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT "问题ID,主键,自增长",
+title VARCHAR(255) NOT NULL COMMENT "问题标题，不为空",
+description TEXT COMMENT "问题描述，可为空",
+code TEXT COMMENT "问题代码，可为空",
+language VARCHAR(50) COMMENT "问题代码语言，可为空",
+views INT UNSIGNED NOT NULL DEFAULT 0 COMMENT "问题浏览次数,不为空,默认值为0",
+votes INT UNSIGNED NOT NULL DEFAULT 0 COMMENT "问题投票次数,不为空,默认值为0",
+created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "问题创建时间，不为空",
+updated_at TIMESTAMP COMMENT "问题更新时间，可为空",
+user_id INT UNSIGNED NOT NULL COMMENT "问题发布者ID,外键,关联用户表中的id字段",
+status TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT "问题状态,不为空,默认值为0,0表示未解决,1表示已解决",
 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE COMMENT "外键约束，当用户被删除时，其发布的问题也会被删除"
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表'COMMENT "问题表";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT "问题表";
 
 CREATE TABLE tag (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 标签ID，主键，自增长
 name VARCHAR(50) UNIQUE NOT NULL -- 标签名称，唯一，不为空
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表' COMMENT "标签表";
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT "标签表";
 
 CREATE TABLE question_tag (
 question_id INT UNSIGNED NOT NULL, -- 问题ID，外键，关联问题表中的id字段
@@ -37,7 +42,7 @@ tag_id INT UNSIGNED NOT NULL, -- 标签ID，外键，关联标签表中的id字�
 PRIMARY KEY (question_id, tag_id), -- 主键，由问题ID和标签ID组成
 FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE, -- 外键约束，当问题被删除时，与之相关的标签也会被删除
 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE -- 外键约束，当标签被删除时，与之相关的问题也会被删除
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表' COMMENT "问题-标签关联表";
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT "问题-标签关联表";
 
 -- 3.回答模块
 CREATE TABLE answer (
@@ -53,7 +58,7 @@ question_id INT UNSIGNED NOT NULL, -- 所回答问题ID，外键，关联问题�
 is_accepted TINYINT UNSIGNED NOT NULL DEFAULT 0, -- 回答是否被采纳，不为空，默认值为0，0表示未被采纳，1表示已被采纳
 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束，当用户被删除时，其回答也会被删除
 FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE -- 外键约束，当问题被删除时，与之相关的回答也会被删除
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表' COMMENT "回答表" ;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT "回答表" ;
 
 --4.评论模块
 CREATE TABLE comment (
@@ -67,7 +72,7 @@ answer_id INT UNSIGNED, -- 所评论回答ID，外键，关联回答表中的id�
 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束，当用户被删除时，其评论也会被删除
 FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE, -- 外键约束，当问题被删除时，与之相关的评论也会被删除
 FOREIGN KEY (answer_id) REFERENCES answer (id) ON DELETE CASCADE -- 外键约束，当回答被删除时，与之相关的评论也会被删除
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表'COMMENT"评论模块";
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT"评论表";
 
 -- 6.通知模块
 CREATE TABLE notification (
@@ -82,10 +87,9 @@ FOREIGN KEY (sender_id) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束�
 FOREIGN KEY (receiver_id) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束，当接收者被删除时，其接收的通知也会被删除
 FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE, -- 外键约束，当问题被删除时，与之相关的通知也会被删除
 FOREIGN KEY (answer_id) REFERENCES answer (id) ON DELETE CASCADE -- 外键约束，当回答被删除时，与之相关的通知也会被删除
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表'COMMENT "通知表";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT "通知表";
 
 -- 7.代码编辑模块
--- 代码表
 CREATE TABLE code (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 代码ID，主键，自增长
 title VARCHAR(200) NOT NULL, -- 代码标题，不为空
@@ -94,9 +98,8 @@ created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 代码创建时间�
 updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 代码更新时间，不为空
 user_id INT UNSIGNED NOT NULL, -- 用户ID，外键，关联用户表中的id字段
 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE -- 外键约束，当用户被删除时，其保存的代码也会被删除
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='代码表';
 
--- 代码分享表
 CREATE TABLE code_share (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 代码分享ID，主键，自增长
 code_id INT UNSIGNED NOT NULL, -- 代码ID，外键，关联代码表中的id字段
@@ -106,19 +109,17 @@ created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 分享时间，不�
 FOREIGN KEY (code_id) REFERENCES code (id) ON DELETE CASCADE, -- 外键约束，当被分享的代码被删除时，其分享信息也会被删除
 FOREIGN KEY (shared_by) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束，当分享者被删除时，其分享的代码信息也会被删除
 FOREIGN KEY (shared_to) REFERENCES user (id) ON DELETE CASCADE -- 外键约束，当被分享者被删除时，其被分享的代码信息也会被删除
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='代码分享表';
 
 -- 社交模块
--- 关注表
 CREATE TABLE follow (
 follower_id INT UNSIGNED NOT NULL, -- 关注者ID，外键，关联用户表中的id字段
 followee_id INT UNSIGNED NOT NULL, -- 被关注者ID，外键，关联用户表中的id字段
 PRIMARY KEY (follower_id, followee_id), -- 主键，由关注者ID和被关注者ID组成
 FOREIGN KEY (follower_id) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束，当关注者被删除时，其关注信息也会被删除
 FOREIGN KEY (followee_id) REFERENCES user (id) ON DELETE CASCADE -- 外键约束，当被关注者被删除时，其被关注信息也会被删除
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='关注表';
 
--- 粉丝表
 CREATE TABLE fan (
 user_id INT UNSIGNED NOT NULL, -- 用户ID，外键，关联用户表中的id字段
 fan_id INT UNSIGNED NOT NULL, -- 粉丝ID，外键，关联用户表中的id字段
@@ -127,7 +128,6 @@ FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE, -- 外键约束，
 FOREIGN KEY (fan_id) REFERENCES user (id) ON DELETE CASCADE -- 外键约束，当粉丝被删除时，其关注信息也会被删除
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='粉丝表';
 
--- 私信表
 CREATE TABLE message (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 私信ID，主键，自增长
 content TEXT NOT NULL, -- 私信内容，不为空
