a70771ae16dcd4a49cc814a3289b353a7d5e8e68
GaoYang <GaoYangBenYang@outlook.com>
2023-04-11 17:37:40 +0800

    📖 添加一些常见状态码

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -1,16 +1,5 @@
 package utils
 
-
-
-// 分类	描述
-// 1xx	信息，服务器收到请求，需要请求者继续执行操作
-// 2xx	成功，操作被成功接收并处理
-// 3xx	重定向，需要进一步的操作以完成请求
-// 4xx	客户端错误，请求包含语法错误或无法完成请求
-// 5xx	服务器错误，服务器在处理请求的过程中发生了错误
-
-
-
 type responseBody struct {
 	Code int
 	Message interface{}
