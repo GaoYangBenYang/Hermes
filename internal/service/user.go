72037c02ac1e53ea972bad2fb64d1a729c5abeda
GaoYang <GaoYangBenYang@outlook.com>
2023-09-25 13:33:51 +0800

    ✨ 完善表结构

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,19 +1,5 @@
 package service
 
 import (
-	"fmt"
-	deo "github.com/GaoYangBenYang/code-fixer/internal/model/deo"
 )
 
-func SelectUserByPhoneNumberOrEmail(phone_number_or_email, password string) error {
-	//根据phone_number_or_email查询数据库判断用户是否存在是否账号异常
-	userPassword, err := deo.SelectUserByPhoneNumberOrEmail(phone_number_or_email)
-	if err == nil && userPassword == password {
-		//判断密码是否正确
-		fmt.Println(userPassword)
-		return nil
-
-	} else {
-		return err
-	}
-}
