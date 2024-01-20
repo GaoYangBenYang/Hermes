bb6aa8465c4041770d72ad750ec8177bd8ac0217
GaoYang <98891158+GaoYangBenYang@users.noreply.github.com>
2024-01-20 20:13:44 +0800

    :zap: 添加依赖和插件 (#11)

--- marshall-web/src/routers/routers.ts
+++ marshall-web/src/routers/routers.ts
@@ -1,15 +1,12 @@
-import VueRouter from 'vue-router';
-
-const Home = {template: '<div>Home</div>'}
-const About = {template: '<div>About</div>'}
-
-const routers = [
-    {path: '/', component: Home},
-    {path: '/about', component: About},
-]
+import * as VueRouter from 'vue-router';
+import Dashboard from '../views/dashboard/index.vue';
+import Login from '../views/login/index.vue';
 
 export const router = VueRouter.createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
     history: VueRouter.createWebHashHistory(),
-    routes: routers, // `routes: routes` 的缩写
-})
+    routes: [
+        {path: '/', component: Dashboard},
+        {path: '/login', component: Login},
+    ]
+});
