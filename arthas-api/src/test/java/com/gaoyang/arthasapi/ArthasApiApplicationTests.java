18bd2bbf186d72a8ca4e93bc86cfa1214e1a4847
GaoYang <GaoYangBenYang@outlook.com>
2024-04-28 16:05:09 +0800

    :tada: Arthas 项目初始化

---
+++ arthas-api/src/test/java/com/gaoyang/arthasapi/ArthasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoyang.arthasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- marshall-web/src/main.ts
+++
@@ -1,6 +0,0 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
-import {router} from './routers/routers.ts'
-
-createApp(App).use(router).mount('#app')
