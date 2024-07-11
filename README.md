adeb766b43383c820f640da9a3d47586ccab1be8
GaoYang <GaoYangBenYang@outlook.com>
2024-07-11 16:06:48 +0800

    项目修正

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Arthas
+# Hermes
 
 在这里写上你的项目名称和 logo（如果有的话）
 
--- hermes-api/src/main/java/com/gaoyang/hermes/HermesApiApplication.java
+++ hermes-api/src/main/java/com/gaoyang/hermes/HermesApiApplication.java
@@ -1,13 +1,13 @@
-package com.gaoyang.arthas;
+package com.gaoyang.hermes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArthasApiApplication {
+public class HermesApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArthasApiApplication.class, args);
+        SpringApplication.run(HermesApiApplication.class, args);
     }
 
 }
