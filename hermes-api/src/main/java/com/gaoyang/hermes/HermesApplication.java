ec563b8f5822d1e6da79a1e586bf83b716f080c5
GaoYang <GaoYangBenYang@outlook.com>
2024-07-11 16:09:02 +0800

    项目启动文件

--- hermes-api/src/main/java/com/gaoyang/hermes/HermesApplication.java
+++ hermes-api/src/main/java/com/gaoyang/hermes/HermesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HermesApiApplication {
+public class HermesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HermesApiApplication.class, args);
+        SpringApplication.run(HermesApplication.class, args);
     }
 
 }
