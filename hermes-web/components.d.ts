148defacf95754c326ff32a14c106ca3d4644632
GaoYang <GaoYangBenYang@outlook.com>
2024-07-25 16:19:25 +0800

    :arrow_up: 引入Element UI

--- hermes-web/components.d.ts
+++ hermes-web/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    LoginButton: typeof import('./src/components/LoginButton.vue')['default']
   }
 }
--- hermes-web/src/App.vue
+++ hermes-web/src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-
+import LoginButton from "./components/LoginButton.vue";
 </script>
 
 <template>
-  <h1 class="text-6xl font-bold underline">
-    Hello world!
-  </h1>
+  <LoginButton/>
 </template>
 
 <style scoped>
