099dada5c38588f3ac14b78788a3932b87d5be39
GaoYang <GaoYangBenYang@outlook.com>
2024-08-05 16:58:03 +0800

    :sparkles: SSO登录组件

---
+++ hermes-web/src/components/Login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+function login() {
+  window.location.replace("http://localhost:8080/oidc/authorize?redirect_uri=http://localhost:8081/code_flow/oidc_op&scope=openid+profile&response_type=code&nonce=cdYrYNLv6wBHlBmZjWxvrQmmD&state=aHR0cDovL3JwLmNvbTo4MDgx&client_id=cnAuY29t");
+}
+
+</script>
+
+<template>
+
+  <div class="card">
+    <button type="button" @click="login">{{ msg }}</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
